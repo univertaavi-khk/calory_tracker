49da324fdf7465b2d7867418a0a404419b233b38
univertaavi-khk <univer.taavi@khk.ee>
2022-01-03 09:32:14 +0200

    Created Controllers and Data Structure

---
+++ app.js
@@ -0,0 +1,43 @@
+// Storage Controller 
+// create later
+
+// Item Controller
+const ItemCtrl = (function() {
+	// Item Constructor
+	const Item = function(id, name, calories) {
+	this.id = id 
+	this.name = name 
+	this.calories = calories
+	}
+	
+	// Data Structure
+	const data = {
+	items: [
+		{id: 0, name: 'Steak Dinner', calories: 1200}, 
+		{id: 1, name: 'Cookie', calories: 400}, 
+		{id: 2, name: 'Eggs', calories: 300}
+	],
+	total: 0
+	}
+	
+	return { 
+		LogData: function() {
+			return data
+		}
+	}
+})();
+
+// UI Controller
+const UICtrl = (function() {
+});
+// App Controller 
+const App = (function(ItemCtrl, UICtrl){
+	return {
+		init: function(){
+			console.log("Initializing App")
+		}
+	}
+}) (ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
